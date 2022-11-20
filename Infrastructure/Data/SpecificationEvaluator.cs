4c455134660151524be26e7e08354d7d3af3a8b8
Joshua Perez <joshuaetperez@gmail.com>
2022-11-20 11:03:06 -0800

    End of section 5

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -15,7 +15,7 @@ namespace Infrastructure.Data
 
             if (spec.Criteria != null)
             {
-                query.Where(spec.Criteria);
+                query = query.Where(spec.Criteria);
             }
 
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
